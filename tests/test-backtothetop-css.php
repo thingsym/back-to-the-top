2ff0f40c5c53e8a38488d7d39754fb36db0a56b1
thingsym <thingsym@gmail.com>
2021-12-09 18:49:48 +0900

    style: fix indent

--- tests/test-backtothetop-css.php
+++ tests/test-backtothetop-css.php
@@ -2,20 +2,19 @@
 
 class BackToTheTop_CSS_Test extends WP_UnitTestCase {
 
-    public function setUp() {
-        parent::setUp();
-        $this->Back_to_the_Top = new Back_to_the_Top();
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->Back_to_the_Top = new Back_to_the_Top();
+	}
 
-    /**
-     * @test
-     * @group css
-     */
-    function css() {
-        $css = $this->Back_to_the_Top->add_css();
+	/**
+	 * @test
+	 * @group css
+	 */
+	function css() {
+		$css = $this->Back_to_the_Top->add_css();
 
-        $this->assertRegExp( '/^<style>.*<\/style>$/', $css );
-
-    }
+		$this->assertRegExp( '/^<style>.*<\/style>$/', $css );
+	}
 
 }
--- tests/test-backtothetop-html.php
+++ tests/test-backtothetop-html.php
@@ -2,51 +2,51 @@
 
 class BackToTheTop_HTML_Test extends WP_UnitTestCase {
 
-    public function setUp() {
-        parent::setUp();
-        $this->Back_to_the_Top = new Back_to_the_Top();
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->Back_to_the_Top = new Back_to_the_Top();
+	}
 
-    /**
-     * @test
-     * @group html
-     */
-    function html() {
-        $html = $this->Back_to_the_Top->add_html();
+	/**
+	 * @test
+	 * @group html
+	 */
+	function html() {
+		$html = $this->Back_to_the_Top->add_html();
 
-        $this->assertRegExp( '/^<a href="#" id="backtothetop-fixed".*>.*<\/a>$/', $html );
-    }
+		$this->assertRegExp( '/^<a href="#" id="backtothetop-fixed".*>.*<\/a>$/', $html );
+	}
 
-    /**
-     * @test
-     * @group html
-     */
-    function html_case_1() {
-        $options = array(
-            'duration' => 400,
-            'easing' => 'swing',
-            'offset' => 0,
-            'fixed-scroll-offset' => 0,
-            'fixed-fadeIn' => 800,
-            'fixed-fadeOut' => 800,
-            'fixed-display' => 'bottom-right',
-            'fixed-top' => 0,
-            'fixed-bottom' => 0,
-            'fixed-left' => 0,
-            'fixed-right' => 0,
+	/**
+	 * @test
+	 * @group html
+	 */
+	function html_case_1() {
+		$options = array(
+			'duration' => 400,
+			'easing' => 'swing',
+			'offset' => 0,
+			'fixed-scroll-offset' => 0,
+			'fixed-fadeIn' => 800,
+			'fixed-fadeOut' => 800,
+			'fixed-display' => 'bottom-right',
+			'fixed-top' => 0,
+			'fixed-bottom' => 0,
+			'fixed-left' => 0,
+			'fixed-right' => 0,
 
-            'label' => 'test test',
-            'font-size' => 140,
-            'font-weight' => 400,
-            'font-color' => '',
-            'font-hover-color' => '',
-            'custom-css' => '',
-        );
+			'label' => 'test test',
+			'font-size' => 140,
+			'font-weight' => 400,
+			'font-color' => '',
+			'font-hover-color' => '',
+			'custom-css' => '',
+		);
 
-        update_option( 'back_to_the_top_options', $options );
-        $html = $this->Back_to_the_Top->add_html();
+		update_option( 'back_to_the_top_options', $options );
+		$html = $this->Back_to_the_Top->add_html();
 
-        $this->assertRegExp( '/^<a href="#" id="backtothetop-fixed".*>test test<\/a>$/', $html );
-    }
+		$this->assertRegExp( '/^<a href="#" id="backtothetop-fixed".*>test test<\/a>$/', $html );
+	}
 
 }
